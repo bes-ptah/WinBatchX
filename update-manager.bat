dc00accc5690401eb90adfe209d601a2721667ad
bes-ptah <77357313+bes-ptah@users.noreply.github.com>
2021-04-26 16:37:47 -0500

    Update update-manager.bat

--- update-manager.bat
+++ update-manager.bat
@@ -11,22 +11,6 @@ IF %_build%==1530 set update.exe=0x01 &exit /b
 SET update=error
 exit /b
 
-:0x00 
-echo You have the latest release/build of WinBatchX.
-pause
-exit
-
-:0x01
-echo You have a supported release/build of WinBatchX.
-echo Another version or build is available.
-pause
-exit
-
-:0x02
-echo You have an unsupported build/release of WinBatchX.
-echo Check out another supported release.
-pause
-exit
 
 
 

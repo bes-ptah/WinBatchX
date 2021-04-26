378df43d9a03d4f90d0a5f1c244c8c698b1a467a
bes-ptah <77357313+bes-ptah@users.noreply.github.com>
2021-04-26 15:16:42 -0500

    Update update-manager.bat

--- update-manager.bat
+++ update-manager.bat
@@ -6,50 +6,6 @@ rem 0x0 Supported.
 rem 0x1 Not latest release.
 rem 0x2 Unsupported.
 
-cls
-echo Please Wait... ^|
-IF %ver-win%==15.0 goto 0x02
-cls
-echo Please Wait... ^/
-IF %ver-win%==1502 goto 0x02
-cls
-echo Please Wait... ^-
-IF %build%==1506 goto 0x02
-cls
-echo Please Wait... ^\
-IF %build%==1508 goto 0x02
-cls
-echo Please Wait... ^|
-IF %build%==1510 goto 0x02
-cls
-echo Please Wait... ^/
-IF %build%==1512 goto 0x01
-cls
-echo Please Wait... ^-
-IF %build%==1514 goto 0x02
-cls
-echo Please Wait... ^\
-IF %build%==1516 goto 0x02
-cls
-echo Please Wait... ^|
-IF %build%==1518 goto 0x01
-cls
-echo Please Wait... ^/
-IF %_build%==1520 goto 0x01
-cls
-echo Please Wait... ^-
-IF %_build%==1522 goto 0x01
-cls
-echo Please Wait... ^\
-IF %_build%==1524 goto 0x01
-cls
-echo Please Wait... ^\
-IF %_build%==1526 goto 0x01
-cls
-echo Please Wait... ^\
-IF %_build%==1528 goto 0x00
-cls
-echo Please Wait... ^\
 IF %_build%==1530 set update.exe=0x01 &exit /b
 SET update=error
 exit /b

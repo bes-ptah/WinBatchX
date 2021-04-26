3d6002882e3e3fa904b34631ffdfa161da4d953c
bes-ptah <77357313+bes-ptah@users.noreply.github.com>
2021-04-26 15:34:17 -0500

    Update update-manager.bat

--- update-manager.bat
+++ update-manager.bat
@@ -6,15 +6,7 @@ rem 0x0 Supported.
 rem 0x1 Not latest release.
 rem 0x2 Unsupported.
 
-ECHO OFF
-
-
-
-rem 0x0 Supported.
-rem 0x1 Not latest release.
-rem 0x2 Unsupported.
-
-echo Please Wait... ^\
+IF %_build%==1532 set update.exe=0x02 &exit /b
 IF %_build%==1530 set update.exe=0x01 &exit /b
 SET update=error
 exit /b
@@ -39,26 +31,6 @@ exit
 
 
 
-IF %_build%==1530 set update.exe=0x01 &exit /b
-SET update=error
-exit /b
-
-:0x00 
-echo You have the latest release/build of WinBatchX.
-pause
-exit
-
-:0x01
-echo You have a supported release/build of WinBatchX.
-echo Another version or build is available.
-pause
-exit
-
-:0x02
-echo You have an unsupported build/release of WinBatchX.
-echo Check out another supported release.
-pause
-exit
 
 
 

ebc9815b6049bf8d036ad87e632e99ef9c4bbc06
bes-ptah <77357313+bes-ptah@users.noreply.github.com>
2021-04-27 08:30:45 -0400

    Update update-manager.bat

--- update-manager.bat
+++ update-manager.bat
@@ -7,7 +7,7 @@ rem 0x1 Not latest release.
 rem 0x2 Unsupported.
 
 
-IF %_build%==1530 set update.exe=0x01 &set REQUEST-action.exe-UPDATE=0x01 &exit /b
+IF %_build%==1530 set update.exe=0x02 &set REQUEST-action.exe-UPDATE=0x02 &exit /b
 IF %_build%==1532 set update.exe=0x00 &set REQUEST-action.exe-UPDATE=0x01 &exit /b
 IF %_build%==1534 set update.exe=0x00 &set REQUEST-action.exe-UPDATE=0x01 &exit /b
 IF %_build%==1536 set update.exe=0x00 &set REQUEST-action.exe-UPDATE=0x01 &exit /b

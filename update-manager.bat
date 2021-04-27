afc93417842206b60ecbf01048e0767d901209b4
bes-ptah <77357313+bes-ptah@users.noreply.github.com>
2021-04-26 22:00:22 -0400

    Update update-manager.bat

--- update-manager.bat
+++ update-manager.bat
@@ -7,7 +7,7 @@ rem 0x1 Not latest release.
 rem 0x2 Unsupported.
 
 IF %_build%==1532 set update.exe=0x02 &exit /b
-IF %_build%==1530 set update.exe=0x01 &exit /b
+IF %_build%==1530 set update.exe=0x02 &exit /b
 SET update=error
 exit /b
 

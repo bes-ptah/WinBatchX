bc04511b8b25e189bb62e01892a8193c788fad90
bes-ptah <77357313+bes-ptah@users.noreply.github.com>
2021-05-12 19:42:10 -0400

    Update update-manager.bat

--- update-manager.bat
+++ update-manager.bat
@@ -11,10 +11,19 @@ IF %_build%==1530 set update.exe=0x02 &set REQUEST-action.exe-UPDATE=0x02 &exit 
 IF %_build%==1532 set update.exe=0x01 &set update-status-action.exe=0x01 &exit /b
 IF %_build%==1534 set update.exe=0x01 &set update-status-action.exe=0x01 &exit /b
 IF %_build%==1536 set update.exe=0x01 &set update-status-action.exe=0x01 &exit /b
-IF %_build%==1538 set update.exe=0x00 &set update-status-action.exe=0x00 &exit /b
+IF %_build%==1538 set update.exe=0x01 &set update-status-action.exe=0x01 &exit /b
 IF %_build%==1540 set update.exe=0x00 &set update-status-action.exe=0x00 &exit /b
+IF %_build%==1542 set update.exe=0x00 &set update-status-action.exe=0x00 &exit /b
 
 rem These builds haven't existed yet. Why did I place them? 
+IF %_build%==1544 set update.exe=0x00 &set update-status-action.exe=0x00 &exit /b
+IF %_build%==1546 set update.exe=0x00 &set update-status-action.exe=0x00 &exit /b
+IF %_build%==1548 set update.exe=0x00 &set update-status-action.exe=0x00 &exit /b
+IF %_build%==1550 set update.exe=0x00 &set update-status-action.exe=0x00 &exit /b
+IF %_build%==1552 set update.exe=0x00 &set update-status-action.exe=0x00 &exit /b
+IF %_build%==1554 set update.exe=0x00 &set update-status-action.exe=0x00 &exit /b
+IF %_build%==1556 set update.exe=0x00 &set update-status-action.exe=0x00 &exit /b
+
 SET update=error
 exit /b
 

000a27f330ad8291610bf369f87dee4c467836b0
bes-ptah <77357313+bes-ptah@users.noreply.github.com>
2021-05-25 09:43:28 -0600

    Update update-manager.bat

--- update-manager.bat
+++ update-manager.bat
@@ -8,10 +8,10 @@ rem 0x2 Unsupported.
 
 
 IF %_build%==1530 set update.exe=0x02 &set REQUEST-action.exe-UPDATE=0x02 &exit /b
-IF %_build%==1532 set update.exe=0x01 &set update-status-action.exe=0x01 &exit /b
-IF %_build%==1534 set update.exe=0x01 &set update-status-action.exe=0x01 &exit /b
-IF %_build%==1536 set update.exe=0x01 &set update-status-action.exe=0x01 &exit /b
-IF %_build%==1538 set update.exe=0x01 &set update-status-action.exe=0x01 &exit /b
+IF %_build%==1532 set update.exe=0x01 &set update-status-action.exe=0x02 &exit /b
+IF %_build%==1534 set update.exe=0x01 &set update-status-action.exe=0x02 &exit /b
+IF %_build%==1536 set update.exe=0x01 &set update-status-action.exe=0x02 &exit /b
+IF %_build%==1538 set update.exe=0x01 &set update-status-action.exe=0x02 &exit /b
 IF %_build%==1540 set update.exe=0x01 &set update-status-action.exe=0x01 &exit /b
 IF %_build%==1542 set update.exe=0x01 &set update-status-action.exe=0x01 &exit /b
 IF %_build%==1544 set update.exe=0x01 &set update-status-action.exe=0x01 &exit /b

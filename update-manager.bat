9300aea7d2008e02d2639cc6c8af2809feb04a86
bes-ptah <77357313+bes-ptah@users.noreply.github.com>
2021-05-26 14:12:08 -0600

    Update update-manager.bat

--- update-manager.bat
+++ update-manager.bat
@@ -18,11 +18,6 @@ IF %_build%==1544 set update.exe=0x02 &set update-status-action.exe=0x02 &exit /
 IF %_build%==1546 set update.exe=0x01 &set update-status-action.exe=0x01 &exit /b
 IF %_build%==1548 set update.exe=0x01 &set update-status-action.exe=0x01 &exit /b
 IF %_build%==1550 set update.exe=0x00 &set update-status-action.exe=0x00 &exit /b
-
-:: IF %_build%==1552 set update.exe=0x00 &set update-status-action.exe=0x00 &exit /b
-:: Build 1552 will be related to Build 1550.
-:: Developers, if you're wondering why- Build 1552 doesn't have the _build variable changed. DON'T Change it. Doesn't exist here.
-
 IF %_build%==1554 set update.exe=0x00 &set update-status-action.exe=0x00 &exit /b
 IF %_build%==1556 set update.exe=0x00 &set update-status-action.exe=0x00 &exit /b
 
